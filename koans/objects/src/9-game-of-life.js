8fff89daa9b4712714e494e4d716e6ef53774484
Graeme Tait <burriko@gmail.com>
2015-06-10 12:22:39 +0100

    Objects koans

--- koans/objects/src/9-game-of-life.js
+++ koans/objects/src/9-game-of-life.js
@@ -1,4 +1,22 @@
 var SAMURAIPRINCIPLE = {};
+SAMURAIPRINCIPLE.cell = {
+  'true': {
+    '0': false,
+    '1': false,
+    '2': true,
+    '3': true,
+    '4': false,
+    '5': false,
+    '6': false,
+    '7': false,
+    '8': false,
+  },
+  'false': {
+    '3': true,
+    '2': false,
+    '4': false,
+  }
+};
 SAMURAIPRINCIPLE.isCellAliveInNextGeneration = function (isCellAlive, numberOfNeighbours) {
-	throw 'Not implemented!';
+  return SAMURAIPRINCIPLE.cell[isCellAlive][numberOfNeighbours];
 };
