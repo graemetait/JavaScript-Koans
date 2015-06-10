f4a473af352fc3c05039e46accedf647b3c97261
Graeme Tait <burriko@gmail.com>
2015-06-10 12:25:34 +0100

    Shorten last exercise

--- koans/objects/src/9-game-of-life.js
+++ koans/objects/src/9-game-of-life.js
@@ -1,22 +1,13 @@
 var SAMURAIPRINCIPLE = {};
 SAMURAIPRINCIPLE.cell = {
   'true': {
-    '0': false,
-    '1': false,
     '2': true,
     '3': true,
-    '4': false,
-    '5': false,
-    '6': false,
-    '7': false,
-    '8': false,
   },
   'false': {
     '3': true,
-    '2': false,
-    '4': false,
   }
 };
 SAMURAIPRINCIPLE.isCellAliveInNextGeneration = function (isCellAlive, numberOfNeighbours) {
-  return SAMURAIPRINCIPLE.cell[isCellAlive][numberOfNeighbours];
+  return SAMURAIPRINCIPLE.cell[isCellAlive][numberOfNeighbours] || false;
 };
