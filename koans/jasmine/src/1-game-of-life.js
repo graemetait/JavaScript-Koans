83362e793fd2a0bf272a86f72b5c08a936ebf2e2
Graeme Tait <burriko@gmail.com>
2015-06-10 10:01:58 +0100

    Game of life tests

--- koans/jasmine/src/1-game-of-life.js
+++ koans/jasmine/src/1-game-of-life.js
@@ -1,2 +1,6 @@
 function isCellAliveInNextGeneration(isCellAlive, numberOfNeighbours) {
+  if (!isCellAlive && numberOfNeighbours === 3) {
+    return true;
+  }
+  return numberOfNeighbours === 2 || numberOfNeighbours === 3;
 }
